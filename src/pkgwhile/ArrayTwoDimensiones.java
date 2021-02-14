82ad381ada809d0d109958c92778d992fbabb5f2
alvaro <alvaromaniatics@hotmail.com>
2021-02-13 18:49:00 -0600

    Arreglo Bidimencional

--- src/pkgwhile/ArrayTwoDimensiones.java
+++ src/pkgwhile/ArrayTwoDimensiones.java
@@ -26,9 +26,46 @@ public static void insertar(int[][]array){
     }
 
 }
+public static void llenarTabla(String [][]array){
+    Scanner s =  new Scanner(System.in);
+    String numero;
+    
+    
+    for (int i = 0; i < array.length; i++) {
+        for (int j = 0; j <array[0].length; j++) {
+            if(array[0][2]!=null){
+            System.out.println("Valor para campo "+array[0][j] );
+            numero = s.nextLine();
+            array[i][j]=numero;
+            }else{
+            System.out.println("Campos");
+            numero = s.nextLine();
+            array[i][j]=numero; 
+            }
+           
+           
+            
+           
+            
+       
+        }
+    }
 
-
+}
+//==========================
 public static void mostrar(int[][]array){
+      for (int i = 0; i < array.length; i++) {
+          System.out.print("|");
+        for (int j = 0; j <array[i].length; j++) {
+           System.out.print(array[i][j]);
+           if (j!=array[i].length-1) System.out.print("\t");
+        }
+        System.out.println("|");
+    }
+}
+//==========================
+
+public static void mostrarTabla(String[][]array){
       for (int i = 0; i < array.length; i++) {
           System.out.print("|");
         for (int j = 0; j <array[i].length; j++) {
@@ -41,9 +78,13 @@ public static void mostrar(int[][]array){
 
     public static void main(String[] args) {
         int array [][] = new int [3][3];//3 filas 4 columnas [][][][]
-        ArrayTwoDimensiones.insertar(array);
-        ArrayTwoDimensiones.mostrar(array);
+//        ArrayTwoDimensiones.insertar(array);
+//        ArrayTwoDimensiones.mostrar(array);
 
-        
+       String array2[][] = new String [3][3];
+        ArrayTwoDimensiones.llenarTabla(array2);
+        ArrayTwoDimensiones.mostrarTabla(array2);
 }
+    
+    
 }
